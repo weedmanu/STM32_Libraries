4e457c6dab0485231167e138385279261ca1eddf
tutoelectroweb <terraspi13@gmail.com>
2025-03-10 15:04:58 +0100

    logo.h

--- STM32_I2C_SSD1306(oled)/ssd1306_conf.h
+++ STM32_I2C_SSD1306(oled)/ssd1306_conf.h
@@ -1,17 +1,17 @@
 /**
  * Private configuration file for the SSD1306 library.
- * This example is configured for STM32F0, I2C and including all fonts.
+ * This example is configured for STM32L1, I2C and including all fonts.
  */
 
 #ifndef __SSD1306_CONF_H__
 #define __SSD1306_CONF_H__
 
 // Choose a microcontroller family
-#define STM32F0
+//#define STM32F0
 //#define STM32F1
 //#define STM32F4
 //#define STM32L0
-//#define STM32L1
+#define STM32L1
 //#define STM32L4
 //#define STM32F3
 //#define STM32H7
@@ -48,9 +48,7 @@
 #define SSD1306_INCLUDE_FONT_7x10
 #define SSD1306_INCLUDE_FONT_11x18
 #define SSD1306_INCLUDE_FONT_16x26
-
 #define SSD1306_INCLUDE_FONT_16x24
-
 #define SSD1306_INCLUDE_FONT_16x15
 
 // The width of the screen can be set using this
