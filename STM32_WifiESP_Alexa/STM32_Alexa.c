887b17af9516a853d08e792912645395682d8ee7
tutoelectroweb <terraspi13@gmail.com>
2025-06-03 21:20:33 +0200

    up

--- STM32_WifiESP_Alexa/STM32_Alexa.c
+++ STM32_WifiESP_Alexa/STM32_Alexa.c
@@ -1,4 +1,10 @@
-// STM32_Alexa.c
+/*
+ * STM32_Alexa.c
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: weedm
+ */
+
 #include "STM32_Alexa.h"
 #include <stdio.h>
 #include <string.h>
@@ -33,12 +39,15 @@ void alexa_init(void)
 	esp01_add_route("/description.xml", alexa_handle_description);
 	esp01_add_route("/api", alexa_handle_api);
 	esp01_add_route("/api/", alexa_handle_api);
+	esp01_add_route("/api/lights", alexa_handle_api);
+	esp01_add_route("/api/lights/", alexa_handle_api);
+	esp01_add_route("/upnp/discovery", alexa_handle_ssdp);
 
 	// Réinitialise le tableau des périphériques
 	memset(g_devices, 0, sizeof(g_devices));
 	g_device_count = 0;
 
-	printf("[ALEXA] Module initialisé, IP: %s\r\n", g_device_ip);
+	alexa_logln("Module initialisé, IP: %s", g_device_ip);
 }
 
 // Ajout d'un périphérique GPIO (ON/OFF)
@@ -60,7 +69,7 @@ int alexa_add_gpio_device(const char *name, GPIO_TypeDef *port, uint16_t pin)
 	device->state = (HAL_GPIO_ReadPin(port, pin) == GPIO_PIN_SET) ? 1 : 0;
 	device->brightness = 255;
 
-	printf("[ALEXA] Périphérique GPIO ajouté: %s (ID: %d)\r\n", name, id);
+	alexa_logln("Périphérique GPIO ajouté: %s (ID: %d)", name, id);
 	return id;
 }
 
@@ -74,7 +83,7 @@ int alexa_add_pwm_device(const char *name, TIM_HandleTypeDef *htim, uint32_t cha
 	alexa_device_t *device = &g_devices[id];
 
 	strncpy(device->name, name, sizeof(device->name) - 1);
-	snprintf(device->uniqueid, sizeof(device->uniqueid), "00:17:88:01:01:%02X:%02X:%02X",
+	snprintf(device->uniqueid, sizeof(device->uniqueid), "00:17:88:01:01:%02X:%02X:%02lX",
 			 id, (unsigned)htim & 0xFF, channel & 0xFF);
 
 	device->type = ALEXA_DEVICE_DIMMABLE;
@@ -83,7 +92,7 @@ int alexa_add_pwm_device(const char *name, TIM_HandleTypeDef *htim, uint32_t cha
 	device->state = 0;
 	device->brightness = 0;
 
-	printf("[ALEXA] Périphérique PWM ajouté: %s (ID: %d)\r\n", name, id);
+	alexa_logln("Périphérique PWM ajouté: %s (ID: %d)", name, id);
 	return id;
 }
 
@@ -92,6 +101,25 @@ void alexa_process(void)
 {
 	// Cette fonction est appelée dans la boucle principale
 	// Le traitement des requêtes est géré par les handlers de route
+}
+
+// Gestionnaire pour la découverte SSDP
+static void alexa_handle_ssdp(int conn_id, const http_parsed_request_t *request)
+{
+	char response[512];
+	snprintf(response, sizeof(response),
+			 "HTTP/1.1 200 OK\r\n"
+			 "CACHE-CONTROL: max-age=86400\r\n"
+			 "EXT:\r\n"
+			 "LOCATION: http://%s:%d/description.xml\r\n"
+			 "SERVER: FreeRTOS/9.0 UPnP/1.0 IpBridge/1.17.0\r\n"
+			 "hue-bridgeid: 001788FFFE100000\r\n"
+			 "ST: urn:schemas-upnp-org:device:basic:1\r\n"
+			 "USN: uuid:2f402f80-da50-11e1-9b23-001788010000\r\n\r\n",
+			 g_device_ip, ALEXA_HTTP_PORT);
+
+	esp01_send_http_response(conn_id, 200, "text/plain", response, strlen(response));
+	alexa_logln("Requête SSDP traitée");
 }
 
 // Gestionnaire pour la description du périphérique
@@ -105,19 +133,20 @@ static void alexa_handle_description(int conn_id, const http_parsed_request_t *r
 			 "<URLBase>http://%s:%d/</URLBase>"
 			 "<device>"
 			 "<deviceType>urn:schemas-upnp-org:device:Basic:1</deviceType>"
-			 "<friendlyName>STM32 Alexa Bridge</friendlyName>"
-			 "<manufacturer>STM32</manufacturer>"
-			 "<manufacturerURL>http://www.st.com/</manufacturerURL>"
-			 "<modelDescription>STM32 Alexa Bridge</modelDescription>"
-			 "<modelName>STM32 Alexa</modelName>"
-			 "<modelNumber>1.0</modelNumber>"
-			 "<serialNumber>001788010000</serialNumber>"
+			 "<friendlyName>Philips hue</friendlyName>"
+			 "<manufacturer>Royal Philips Electronics</manufacturer>"
+			 "<manufacturerURL>http://www.philips.com</manufacturerURL>"
+			 "<modelDescription>Philips hue Personal Wireless Lighting</modelDescription>"
+			 "<modelName>Philips hue bridge 2015</modelName>"
+			 "<modelNumber>BSB002</modelNumber>"
+			 "<serialNumber>001788102201</serialNumber>"
 			 "<UDN>uuid:2f402f80-da50-11e1-9b23-001788010000</UDN>"
 			 "</device>"
 			 "</root>",
 			 g_device_ip, ALEXA_HTTP_PORT);
 
 	esp01_send_http_response(conn_id, 200, "text/xml", response, strlen(response));
+	alexa_logln("Description du périphérique envoyée");
 }
 
 // Gestionnaire pour l'API Hue
@@ -152,11 +181,44 @@ static void alexa_handle_api(int conn_id, const http_parsed_request_t *request)
 	ptr += sprintf(ptr, "}}");
 
 	esp01_send_http_response(conn_id, 200, "application/json", response, strlen(response));
+	alexa_logln("Liste des périphériques envoyée");
 }
 
 // Gestionnaire pour les périphériques
 static void alexa_handle_lights(int conn_id, const http_parsed_request_t *request)
 {
+	// Afficher le chemin complet pour le débogage
+	alexa_logln("Chemin de la requête lights: %s", request->path);
+
+	// Si l'URL est exactement "/api/lights" ou "/api/lights/", retourne la liste complète
+	if (strcmp(request->path, "/api/lights") == 0 || strcmp(request->path, "/api/lights/") == 0)
+	{
+		// Réponse avec la liste des périphériques
+		char response[1024];
+		char *ptr = response;
+		ptr += sprintf(ptr, "{");
+
+		for (int i = 0; i < g_device_count; i++)
+		{
+			ptr += sprintf(ptr, "\"%d\":{\"state\":{\"on\":%s,\"bri\":%d,\"hue\":0,\"sat\":0,\"effect\":\"none\",\"xy\":[0,0],\"ct\":0,\"alert\":\"none\",\"colormode\":\"hs\",\"reachable\":true},\"type\":\"%s\",\"name\":\"%s\",\"modelid\":\"LWB010\",\"manufacturername\":\"Philips\",\"uniqueid\":\"%s\",\"swversion\":\"1.0.0\"}",
+						   i + 1,
+						   g_devices[i].state ? "true" : "false",
+						   g_devices[i].brightness,
+						   g_devices[i].type == ALEXA_DEVICE_DIMMABLE ? "Dimmable light" : "On/Off light",
+						   g_devices[i].name,
+						   g_devices[i].uniqueid);
+
+			if (i < g_device_count - 1)
+				ptr += sprintf(ptr, ",");
+		}
+
+		ptr += sprintf(ptr, "}");
+
+		esp01_send_http_response(conn_id, 200, "application/json", response, strlen(response));
+		alexa_logln("Liste des périphériques envoyée");
+		return;
+	}
+
 	// Extrait l'ID du périphérique de l'URL
 	char device_id_str[8] = {0};
 	const char *id_start = strstr(request->path, "/lights/");
@@ -173,6 +235,8 @@ static void alexa_handle_lights(int conn_id, const http_parsed_request_t *reques
 			strcpy(device_id_str, id_start);
 		}
 	}
+
+	alexa_logln("ID du périphérique extrait: %s", device_id_str);
 
 	// Si c'est une requête PUT pour contrôler un périphérique
 	if (strcmp(request->method, "PUT") == 0 && strstr(request->path, "/state"))
@@ -197,16 +261,23 @@ static void alexa_handle_lights(int conn_id, const http_parsed_request_t *reques
 				 device->uniqueid);
 
 		esp01_send_http_response(conn_id, 200, "application/json", response, strlen(response));
+		alexa_logln("Informations du périphérique %d envoyées", device_id);
 	}
 	else
 	{
 		esp01_send_http_response(conn_id, 404, "application/json", "{\"error\":\"Device not found\"}", 26);
+		alexa_logln("Périphérique %d non trouvé", device_id);
 	}
 }
 
 // Gestionnaire pour le contrôle des périphériques
 static void alexa_handle_light_control(int conn_id, const http_parsed_request_t *request)
 {
+	// Afficher le chemin complet pour le débogage
+	alexa_logln("Chemin de la requête de contrôle: %s", request->path);
+	alexa_logln("Méthode: %s", request->method);
+	alexa_logln("Headers: %s", request->headers_buf);
+
 	// Extrait l'ID du périphérique de l'URL
 	char device_id_str[8] = {0};
 	const char *id_start = strstr(request->path, "/lights/");
@@ -218,55 +289,82 @@ static void alexa_handle_light_control(int conn_id, const http_parsed_request_t 
 		{
 			strncpy(device_id_str, id_start, id_end - id_start);
 		}
+		else
+		{
+			strcpy(device_id_str, id_start);
+		}
 	}
+
+	alexa_logln("ID du périphérique extrait: %s", device_id_str);
 
 	int device_id = atoi(device_id_str) - 1;
 	if (device_id < 0 || device_id >= g_device_count)
 	{
 		esp01_send_http_response(conn_id, 404, "application/json", "{\"error\":\"Device not found\"}", 26);
+		alexa_logln("Périphérique %d non trouvé pour contrôle", device_id);
 		return;
 	}
 
-	// Parse le corps de la requête pour extraire les commandes
-	const char *body_start = strstr(request->headers_buf, "\r\n\r\n");
-	if (body_start)
+	// Afficher le contenu complet pour le débogage
+	alexa_logln("Contenu de la requête: %s", request->headers_buf);
+
+	// Déterminer l'état à partir de la commande
+	uint8_t new_state = g_devices[device_id].state; // État par défaut
+	uint8_t new_brightness = g_devices[device_id].brightness; // Luminosité par défaut
+	uint8_t brightness_changed = 0;
+	uint8_t state_changed = 0;
+
+	// Détection des commandes ON/OFF
+	if (strstr(request->headers_buf, "on"))
 	{
-		body_start += 4; // Saute "\r\n\r\n"
-
-		uint8_t new_state = g_devices[device_id].state;
-		uint8_t new_brightness = g_devices[device_id].brightness;
-
-		// Cherche la commande "on"
-		const char *on_cmd = strstr(body_start, "\"on\":");
-		if (on_cmd)
+		if (strstr(request->headers_buf, "false"))
 		{
-			new_state = (strstr(on_cmd, "true") != NULL) ? 1 : 0;
+			new_state = 0;
+			state_changed = 1;
+			alexa_logln("Commande OFF détectée");
 		}
-
-		// Cherche la commande "bri" (brightness)
-		const char *bri_cmd = strstr(body_start, "\"bri\":");
-		if (bri_cmd)
+		else
 		{
-			bri_cmd += 6; // Saute "\"bri\":"
-			new_brightness = atoi(bri_cmd);
+			new_state = 1;
+			state_changed = 1;
+			alexa_logln("Commande ON détectée");
 		}
-
-		// Met à jour l'état du périphérique
-		alexa_update_device_state(device_id, new_state, new_brightness);
-
-		// Répond avec succès
-		char response[256];
-		snprintf(response, sizeof(response),
-				 "[{\"success\":{\"/lights/%d/state/on\":%s}}]",
-				 device_id + 1,
-				 new_state ? "true" : "false");
-
-		esp01_send_http_response(conn_id, 200, "application/json", response, strlen(response));
 	}
-	else
+	
+	// Recherche d'une commande de luminosité
+	const char *bri_start = strstr(request->headers_buf, "\"bri\":");
+	if (bri_start && g_devices[device_id].type == ALEXA_DEVICE_DIMMABLE)
 	{
-		esp01_send_http_response(conn_id, 400, "application/json", "{\"error\":\"Invalid request\"}", 25);
+		bri_start += 6; // Longueur de "\"bri\":"
+		new_brightness = (uint8_t)atoi(bri_start);
+		brightness_changed = 1;
+		
+		// Si la luminosité est > 0, on s'assure que l'état est ON
+		if (new_brightness > 0)
+		{
+			new_state = 1;
+			state_changed = 1;
+		}
+		
+		alexa_logln("Commande de luminosité détectée: %d", new_brightness);
 	}
+
+	// Mettre à jour l'état du périphérique
+	alexa_logln("Mise à jour de l'état: device_id=%d, state=%d, brightness=%d", 
+		device_id, new_state, new_brightness);
+	alexa_update_device_state(device_id, new_state, new_brightness);
+
+	// Générer la réponse avec l'ID du périphérique correct
+	char response[256];
+	
+	// Utiliser sprintf pour inclure l'ID correct du périphérique
+	sprintf(response, "[{\"success\":{\"/lights/%d/state/on\":%s}}]", 
+		device_id + 1, 
+		new_state ? "true" : "false");
+
+	esp01_send_http_response(conn_id, 200, "application/json", response, strlen(response));
+	alexa_logln("Contrôle du périphérique %d effectué (état: %d, luminosité: %d)", 
+		device_id, new_state, new_brightness);
 }
 
 // Met à jour l'état d'un périphérique
@@ -286,8 +384,8 @@ static void alexa_update_device_state(int device_id, uint8_t state, uint8_t brig
 		HAL_GPIO_WritePin(device->gpio_port, device->gpio_pin,
 						  state ? GPIO_PIN_SET : GPIO_PIN_RESET);
 
-		printf("[ALEXA] Périphérique %s (%d) mis à %s\r\n",
-			   device->name, device_id, state ? "ON" : "OFF");
+		alexa_logln("Périphérique %s (%d) mis à %s",
+					device->name, device_id, state ? "ON" : "OFF");
 	}
 	else if (device->type == ALEXA_DEVICE_DIMMABLE)
 	{
@@ -296,7 +394,7 @@ static void alexa_update_device_state(int device_id, uint8_t state, uint8_t brig
 		{
 			// Éteint
 			__HAL_TIM_SET_COMPARE(device->htim, device->channel, 0);
-			printf("[ALEXA] Périphérique %s (%d) éteint\r\n", device->name, device_id);
+			alexa_logln("Périphérique %s (%d) éteint", device->name, device_id);
 		}
 		else
 		{
@@ -305,8 +403,8 @@ static void alexa_update_device_state(int device_id, uint8_t state, uint8_t brig
 			uint32_t pulse = (period * brightness) / 255;
 			__HAL_TIM_SET_COMPARE(device->htim, device->channel, pulse);
 
-			printf("[ALEXA] Périphérique %s (%d) réglé à %d%%\r\n",
-				   device->name, device_id, (brightness * 100) / 255);
+			alexa_logln("Périphérique %s (%d) réglé à %d%%",
+						device->name, device_id, (brightness * 100) / 255);
 		}
 	}
 }
@@ -320,4 +418,27 @@ static int alexa_find_device_by_id(const char *id)
 			return i;
 	}
 	return -1;
+}
+
+// Fonction publique pour la découverte
+void alexa_handle_discovery(void)
+{
+	alexa_logln("Envoi d'une annonce de découverte SSDP");
+
+	// Exemple d'annonce SSDP (broadcast UDP sur le port 1900)
+	const char *ssdp_msg =
+		"NOTIFY * HTTP/1.1\r\n"
+		"HOST: 239.255.255.250:1900\r\n"
+		"CACHE-CONTROL: max-age=100\r\n"
+		"LOCATION: http://%s:%d/description.xml\r\n"
+		"SERVER: FreeRTOS/9.0 UPnP/1.0 IpBridge/1.17.0\r\n"
+		"hue-bridgeid: 001788FFFE100000\r\n"
+		"NT: upnp:rootdevice\r\n"
+		"NTS: ssdp:alive\r\n"
+		"USN: uuid:2f402f80-da50-11e1-9b23-001788010000::upnp:rootdevice\r\n\r\n";
+
+	char buffer[512];
+	snprintf(buffer, sizeof(buffer), ssdp_msg, g_device_ip, ALEXA_HTTP_PORT);
+
+	esp01_send_udp_broadcast(1900, buffer, strlen(buffer));
 }
--- STM32_WifiESP_Alexa/STM32_Alexa.h
+++ STM32_WifiESP_Alexa/STM32_Alexa.h
@@ -1,9 +1,26 @@
-// STM32_Alexa.h
+/*
+ * STM32_Alexa.h
+ *
+ *  Created on: Jun 3, 2025
+ *      Author: weedm
+ */
+
 #ifndef STM32_ALEXA_H
 #define STM32_ALEXA_H
 
 #include "STM32_WifiESP.h"
 #include "main.h"
+
+// Macros de débogage
+#define ALEXA_DEBUG 1
+
+#if ALEXA_DEBUG
+#define alexa_logln(fmt, ...) printf("[ALEXA] " fmt "\r\n", ##__VA_ARGS__)
+#define alexa_log(fmt, ...) printf("[ALEXA] " fmt, ##__VA_ARGS__)
+#else
+#define alexa_logln(fmt, ...)
+#define alexa_log(fmt, ...)
+#endif
 
 // Types de périphériques supportés
 typedef enum
@@ -26,6 +43,9 @@ typedef struct
 	uint8_t brightness;		  // Luminosité (0-255, pour type DIMMABLE)
 } alexa_device_t;
 
+// Ces fonctions sont implémentées dans le fichier .c
+// Ne pas déclarer les fonctions statiques dans le fichier d'en-tête
+
 // Initialisation du module Alexa
 void alexa_init(void);
 
@@ -37,5 +57,10 @@ int alexa_add_pwm_device(const char *name, TIM_HandleTypeDef *htim, uint32_t cha
 
 // Traitement des requêtes Alexa (à appeler dans la boucle principale)
 void alexa_process(void);
+
+// Déclaration des fonctions pour la découverte et le contrôle des appareils
+void alexa_handle_discovery(void);
+
+void alexa_send_ssdp_notify(void);
 
 #endif // STM32_ALEXA_H
